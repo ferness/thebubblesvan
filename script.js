c8cad08a08af8a381bc4e12890a668d708d9232d
ferness <92421417+ferness@users.noreply.github.com>
2023-07-21 20:01:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+        //vybrat všechny elementy s třídou .GaleryImg
+        document.querySelectorAll('.GaleryImg').forEach(image =>{
+          image.onclick = () =>{
+              document.querySelector('.popup-image').style.display = 'block';
+              document.querySelector('#popup-img').src = image.getAttribute("src");
+          }
+      });
+
+          document.querySelector('.popup-image span').onclick = () =>{
+          document.querySelector('.popup-image').style.display = 'none';
+
+      };
+      
+      
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+//* Get the button
+//let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+//window.onscroll = function() {scrollFunction()};
+
+//function scrollFunction() {
+//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//    mybutton.style.display = "block";
+//  } else {
+//    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+//function topFunction() {
+  //document.body.scrollTop = 0;
+  //document.documentElement.scrollTop = 0;
+//}
+
+
+// */
